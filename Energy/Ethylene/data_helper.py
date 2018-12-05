7bbe303c3682fb57205a7dde8a8714e1fad59ae7
coderGray <m13070193321@163.com>
2018-12-05 11:36:53 +0800

    finished data_helper

--- Energy/Ethylene/data_helper.py
+++ Energy/Ethylene/data_helper.py
@@ -31,8 +31,21 @@ def load_data(path):
         y.append(data[i][5:])
     return X, y
 
+#generator batch data with shuffled
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
-    
+    data = np.array(data)
+    data_size = len(data)
+    num_batchs_per_epoch = int((data_size - 1) / batch_size) + 1
+    for epoch in range(num_epochs):
+        #shuffled data every epoch
+        if shuffle:
+            data_shufled = np.random.permutation(data)
+        else:
+            data_shufled = data
+        for num_batch in range(num_batchs_per_epoch):
+            start_index = num_batch * batch_size
+            end_index = min((num_batch + 1) * batch_size, data_size)
+            yield data_shufled[start_index : end_index]
 
 
 #_, y = load_data('normalized.txt')
