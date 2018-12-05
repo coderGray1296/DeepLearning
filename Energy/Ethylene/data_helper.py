aea2f5fdd9c534ca9f91af2066fa48023d296936
coderGray <m13070193321@163.com>
2018-12-05 11:26:18 +0800

    update

--- Energy/Ethylene/data_helper.py
+++ Energy/Ethylene/data_helper.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-def load_data(path):
+def save_normalized_data(path):
     #load data from path
     data = np.loadtxt(path)
     data = normalize(data)
-    np.savetxt('normalized.txt', data,)
+    np.savetxt('normalized.txt', data)
 
 def normalize(data):
     min = np.argmin(data, axis=0)
@@ -22,4 +22,17 @@ def normalize(data):
         data[i] = (data[i] - min_value) / (max_value - min_value)
     return data
 
-load_data('data.txt')
+def load_data(path):
+    data = np.loadtxt(path).tolist()
+    X = []
+    y = []
+    for i in range(len(data)):
+        X.append(data[i][0:5])
+        y.append(data[i][5:])
+    return X, y
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    
+
+
+#_, y = load_data('normalized.txt')
