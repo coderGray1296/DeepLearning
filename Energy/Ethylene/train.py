f9f3989170135f5116e25b9c7278d410a9e4f462
coderGray <m13070193321@163.com>
2019-02-25 16:14:08 +0800

    finish train.py and draw.py

--- Energy/Ethylene/train.py
+++ Energy/Ethylene/train.py
@@ -6,6 +6,7 @@ import os
 from model import CNN
 import data_helper
 from tensorflow.contrib import learn
+import draw
 
 #设置训练集和测试集为4：1
 test_sample_percentage = 0.2
@@ -56,6 +57,53 @@ with tf.Graph().as_default():
         # 初始化所有变量
         sess.run(tf.global_variables_initializer())
 
+        #training step
         def train_step(x_batch, y_batch):
-            
+            feed_dict = {
+                cnn.input_x: x_batch,
+                cnn.input_y: y_batch,
+                cnn.dropout_keep_prob: dropout_keep_prob
+            }
+            _, step, loss, accuracy = sess.run(
+                [train_op, global_step, cnn.loss, cnn.accuracy], feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            return loss, accuracy
 
+        #testing step
+        def test_step(x_batch, y_batch):
+            feed_dict = {
+                cnn.input_x: x_batch,
+                cnn.input_y: y_batch,
+                cnn.dropout_keep_prob: 1.0
+            }
+            step, loss, accuracy = sess.run([global_step, cnn.loss, cnn.accuracy],
+                                                       feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            return loss, accuracy
+
+        batches = data_helper.batch_iter(list(zip(x_train, y_train)), batch_size, num_epochs)
+
+        train_loss_all = []
+        train_accuracy_all = []
+        test_loss_all = []
+        test_accuracy_all = []
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            loss_train, accuracy_train = train_step(x_batch, y_batch)
+            train_loss_all.append(loss_train)
+            train_accuracy_all.append(accuracy_train)
+            current_step = tf.train.global_step(sess, global_step)
+            #每evaluate_every进行一次测试
+            if current_step % evaluate_every == 0:
+                print('\nTesting:')
+                loss_test, accuracy_test = test_step(x_test, y_test)
+                test_loss_all.append(loss_test)
+                test_accuracy_all.append(accuracy_test)
+                print("")
+        # draw picture for loss and accuracy of test and train
+        draw.draw_picture('train', train_accuracy_all, train_loss_all)
+        draw.draw_picture('test', test_accuracy_all, test_loss_all)
+
+        print('modelling finished!')
